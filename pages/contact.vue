c3012316d1b295b22ad2bc889dc2942f094fad94
legalito <63124952+legalito@users.noreply.github.com>
2024-02-15 14:39:43 +0100

    Feat/add page contact (#3)
    * Create contact.vue
    
    * Update contact.vue
    
    * up
    
    * up
    
    ---------
    
    Co-authored-by: Corentin DANVIN <91325753+iBySnoW@users.noreply.github.com>

--- pages/contact.vue
+++ pages/contact.vue
@@ -2,5 +2,11 @@
     <div>
         <h1>Contact</h1>
         <p>Send us a message!</p>
+        <form>
+                    <input type="text" placeholder="Name">
+                    <input type="email" placeholder="Email">
+                    <textarea placeholder="Message"></textarea>
+                    <button type="submit">Send</button>
+        </form>
     </div>
 </template>
