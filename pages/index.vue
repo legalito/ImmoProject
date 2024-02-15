e33be75b241855d1953e3bab5bb8059857634cb6
Corentin DANVIN <91325753+iBySnoW@users.noreply.github.com>
2024-02-15 14:36:26 +0100

    feat/add content for home page (#2)

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,27 @@
 <template>
-    <div>
-      <NuxtWelcome />
+     <h1>LEGALIMO</h1>
+     <p>Les meilleurs bien, des prix incroyables, le tout dans la légalité</p>
+
+     <div>
+       <div>
+         <h2>Titre</h2>
+         <p>description</p>
+         <button>détails</button>
+       </div>
+       <div>
+         <h2>Titre</h2>
+         <p>description</p>
+         <button>détails</button>
+       </div>
+       <div>
+         <h2>Titre</h2>
+         <p>description</p>
+         <button>détails</button>
+       </div>
+       <div>
+         <h2>Titre</h2>
+         <p>description</p>
+         <button>détails</button>
+       </div>
     </div>
   </template>
