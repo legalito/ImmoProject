f845a55318a504990d5b5b01428a1fecc34e8120
legalito <63124952+legalito@users.noreply.github.com>
2024-02-29 15:11:23 +0100

    test

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h1>Contact</h1>
+        <h1></h1>
         <p>Send us a message!</p>
         <form>
                     <input type="text" name="name" placeholder="Name">
